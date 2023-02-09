1fefed4d519ee2828500503e2770276d4f4a56cf
YuxinFang99 <122831673+YuxinFang99@users.noreply.github.com>
2023-02-08 23:04:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-iadigd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-iadigd)
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent} from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule
+  ],
+  bootstrap: [ AppComponent ],
+  providers: []
+
+})
+export class AppModule { }
